7ae0b6bc57c3d079ec5fb6277f931e50a2daf052
Gaurav <gaurav.chauhan@vlinkinfo.com>
2022-12-01 13:40:41 +0530

    Initial Commit.

---
+++ src/main/java/com/gch/javaapachekafka/JavaApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gch.javaapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApacheKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApacheKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gch/javaapachekafka/JavaApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gch.javaapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApacheKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
